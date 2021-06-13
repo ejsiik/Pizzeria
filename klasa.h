e82330659d982bf764586aa8fb2bdb3f397427ba
ejsiik <72543889+ejsiik@users.noreply.github.com>
2021-06-13 17:51:10 +0200

    Create klasa.h

---
+++ klasa.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Pizzeria;
+void pause();
+void login();
+void powitanie();
+void plik(string pizza, double cena, bool czyNaWynos, string adres = "");
+Pizzeria* DodajZamowienie(Pizzeria*& head, string pizza, double cena, bool czyNaWynos, string adres, int& nr2);
+Pizzeria* ZnajdzZamowienie(Pizzeria* head, int nr_zam);
+Pizzeria* ZnajdzZamowienie2(Pizzeria* head, string pizza);
+Pizzeria* czyIstnieje(Pizzeria* head, int  nr_zam);
+void usun(Pizzeria*& head, int& nr_zam);
+void wypisz(Pizzeria* head);
+void wyczysc(Pizzeria*& head);
