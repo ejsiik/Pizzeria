683e33da78baaab81a9bae645cba497b08bd84bd
ejsiik <72543889+ejsiik@users.noreply.github.com>
2021-06-16 09:56:25 +0200

    Update klasa.h

--- klasa.h
+++ klasa.h
@@ -5,9 +5,10 @@ using namespace std;
 
 class Pizzeria;
 void pause();
+void blad();
 void login();
 void powitanie();
-void plik(string pizza, double cena, bool czyNaWynos, string adres = "");
+void plik(int nr2, string pizza, double cena, bool czyNaWynos, string adres = "");
 Pizzeria* DodajZamowienie(Pizzeria*& head, string pizza, double cena, bool czyNaWynos, string adres, int& nr2);
 Pizzeria* ZnajdzZamowienie(Pizzeria* head, int nr_zam);
 Pizzeria* ZnajdzZamowienie2(Pizzeria* head, string pizza);
