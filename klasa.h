84babae5cc71880f993f107032dab0a033e63230
ejsiik <72543889+ejsiik@users.noreply.github.com>
2021-06-18 11:00:47 +0200

    Update klasa.h

--- klasa.h
+++ klasa.h
@@ -1,6 +1,11 @@
 #pragma once
 #include <iostream>
-#include <string>
+#include <iomanip> //setw
+#include <string> //string
+#include <Windows.h> //sleep, system
+#include <fstream> //pliki
+#include <conio.h> //_getch()
+
 using namespace std;
 
 class Pizzeria;
@@ -9,10 +14,10 @@ void blad();
 void login();
 void powitanie();
 void plik(int nr2, string pizza, double cena, bool czyNaWynos, string adres = "");
-Pizzeria* DodajZamowienie(Pizzeria*& head, string pizza, double cena, bool czyNaWynos, string adres, int& nr2);
+Pizzeria* DodajZamowienie(Pizzeria*& head, string pizza, double cena, bool czyNaWynos, string adres, int& nr2, double& cenaSuma);
 Pizzeria* ZnajdzZamowienie(Pizzeria* head, int nr_zam);
 Pizzeria* ZnajdzZamowienie2(Pizzeria* head, string pizza);
 Pizzeria* czyIstnieje(Pizzeria* head, int  nr_zam);
-void usun(Pizzeria*& head, int& nr_zam);
+void usun(Pizzeria*& head, int& nr_zam, double& cenaSuma);
 void wypisz(Pizzeria* head);
 void wyczysc(Pizzeria*& head);
